c88a35a0731a621d7e9124cd232e8390c4443d9a
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2024-01-11 19:03:24 -0300

    feat: add app

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const btnGeneratePassword = document.querySelector(".btn-generate-password");
+const input = document.querySelector("#input-password");
+
+btnGeneratePassword.addEventListener("click", () => {
+  input.value = generatePassword();
+  console.log("oi");
+});
+
+input.addEventListener("click", () => {
+  copiarParaAreaDeTransferencia(input);
+});
+
+function generatePassword(comprimento = 20) {
+  const caracteres =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
+  let senha = "";
+
+  for (let i = 0; i < comprimento; i++) {
+    const randomIndex = Math.floor(Math.random() * caracteres.length);
+    senha += caracteres[randomIndex];
+  }
+
+  return senha;
+}
+
+function copiarParaAreaDeTransferencia(input) {
+  if (input.value === "") return;
+
+  // Seleciona o conteúdo do input
+  input.select();
+
+  // Copia o conteúdo para a área de transferência
+  document.execCommand("copy");
+
+  // Deseleciona o input (opcional)
+  input.setSelectionRange(0, 0);
+
+  // Exibe uma mensagem ou realiza outras ações após a cópia (opcional)
+  alert("Texto copiado para a área de transferência!");
+}
